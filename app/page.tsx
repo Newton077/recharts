8a3fdb410e00e7074b66f7e09558dc325f764ea5
eder <eder077@proton.me>
2024-02-09 22:13:46 -0600

    dashboard
    instalacion de paqueterias y arquitectura del dashboard

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { LineChart } from "recharts";
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+        <p>hola perrillos</p>
+        
+      </div>
+    </main>
+  );
+}
